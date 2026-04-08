6ee758ada85b11bf6b23cc8c235fb2520a88fbdf
Piyushrajput2626 <rajputpiyush2602@gmail.com>
2026-04-08 14:14:39 +0530

    Matrix  Main.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Main {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,11 +13,13 @@ public class Main {
                 A[i][j] = sc.nextInt();
             }
         }
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 C[i][j] = 0;
