f3c03ed7279c92a32406714eb16fde2a198716ad
Piyushrajput2626 <rajputpiyush2602@gmail.com>
2026-04-08 13:59:09 +0530

    Spiral  Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -3,13 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             int num = 1;
-
             for (int j = 0; j <= i; j++) {
                 System.out.print(num);
-
                 if (j < i) {
                     System.out.print(" ");
                 }
