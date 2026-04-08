4f540f6cb91b46bdd8bbf6d5213bdaf7a67578c4
Piyushrajput2626 <rajputpiyush2602@gmail.com>
2026-04-08 14:06:54 +0530

    Fibonacci  Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int a = 0, b = 1;
         for (int i = 0; i < n; i++) {
             System.out.print(a);
+            
             if (i != n - 1) {
                 System.out.print(" ");
             }
